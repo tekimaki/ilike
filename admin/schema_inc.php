66f66061e3e1d9f7e4a2e7c957a9fc6afa8077ac
Nick Palmer <nick@sluggardy.net>
2010-08-25 11:58:52 -0400

    Fix installer reference.

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -1,15 +1,15 @@
 <?php
-global $gBitInstaller;
-$gBitInstaller->registerPackageInfo( ILIKE_PKG_NAME, array(
+global $gBitSystem;
+$gBitSystem->registerPackageInfo( ILIKE_PKG_NAME, array(
 	'description' => "A very simple search engine. Only makes sense on small sites since it searches the database directly without any abstraction layer.",
 	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
 ) );
 
-$gBitInstaller->registerUserPermissions( ILIKE_PKG_NAME, array(
+$gBitSystem->registerUserPermissions( ILIKE_PKG_NAME, array(
 	array( 'p_ilike_search', 'Can search your site using the search engine', 'basic', ILIKE_PKG_NAME),
 ) );
 
 // Requirements
-$gBitInstaller->registerRequirements( ILIKE_PKG_NAME, array(
+$gBitSystem->registerRequirements( ILIKE_PKG_NAME, array(
     'liberty' => array( 'min' => '2.1.4' ),
 ));
